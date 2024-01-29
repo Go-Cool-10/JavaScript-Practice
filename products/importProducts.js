051967a49f457b8bf3677bd48ecd54f778c6a0aa
Go-Cool-10 <gogulakrishnanparanthaman@gmail.com>
2024-01-29 14:08:53 +0530

    new commit

---
+++ products/importProducts.js
@@ -0,0 +1,9 @@
+import products from "./products.js";
+
+let container = document.getElementById("container");
+
+products.forEach((product) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = `${product.title} ---- $${product.price}`;
+  container.appendChild(listItem);
+});
