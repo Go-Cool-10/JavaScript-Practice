a379baebb57074018740058ebbb9b2075cf9c570
Go-Cool-10 <gogulakrishnanparanthaman@gmail.com>
2024-01-29 13:42:09 +0530

    new commit

---
+++ ex-imp/driver.js
@@ -0,0 +1,6 @@
+import { add, sub, div, mul } from "./mathFunction.js";
+
+console.log(add(5, 6));
+console.log(sub(6, 2));
+console.log(div(4, 2));
+console.log(mul(2, 2));
---
+++ ex-imp/mathFunction.js
@@ -0,0 +1,10 @@
+let add = (a, b) => a + b;
+let sub = (a, b) => a - b;
+let div = (a, b) => a / b;
+let mul = (a, b) => a * b;
+
+// to export single function
+//export default add;
+
+// to export multiple function
+export { add, sub, div, mul };
