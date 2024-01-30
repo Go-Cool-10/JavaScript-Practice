337495968df0d05096e38dc6dee1b326bf9a2bb5
Go-Cool-10 <gogulakrishnanparanthaman@gmail.com>
2024-01-30 12:29:44 +0530

    checkbox image

--- products/importProducts.js
+++ products/importProducts.js
@@ -1,9 +1,27 @@
 import products from "./products.js";
 
-let container = document.getElementById("container");
-
+let display = " ";
 products.forEach((product) => {
-  const listItem = document.createElement("li");
-  listItem.innerHTML = `<input type="checkbox"> ${product.title} - $${product.price}`;
-  container.appendChild(listItem);
+  display =
+    display +
+    `<input type="checkbox" class="mycheckbox" value="${product.image}"> ${product.title} - $${product.price} <br>`;
+
+  document.getElementById("container").innerHTML = display;
+
+  let checkboxes = document.querySelectorAll(".mycheckbox");
+  for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener("change", displaySelectedValue);
+  }
+
+  function displaySelectedValue() {
+    let selectedValue = " ";
+
+    for (let i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+        selectedValue = selectedValue + `<img src="${checkboxes[i].value}" alt="image" height="200px" width="200px"> <br>`;
+      }
+    }
+
+    document.getElementById("selector").innerHTML = selectedValue;
+  }
 });
